612f6e9ab8ff015026d9edf1887a6958167f7d44
BrendVillegas <148505665+BrendVillegas@users.noreply.github.com>
2023-12-12 17:40:37 -0600

    Update app.component.ts

--- comidas/src/app/app.component.ts
+++ comidas/src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
   //#region Variables
   comidas: Comida[] = [
     {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
-    {desayuno: "Huevo con arroz", comida: "Verduras al vapor", cena: "Quesadillas"},
+    {desayuno: "Chilaquiles", comida: "Saldeado de Res", cena: "Choriqueso"},
+    {desayuno: "Torta Española", comida: "Filete de Pescado", cena: "Tacos Pastor"},
+    {desayuno: "Coctel de Frutas", comida: "Hamburguesa", cena: "Pechuga Asada"},
+    {desayuno: "Jugo Verde", comida: "Carnita Asada", cena: "Sardina Portola"},
+    {desayuno: "Huevo con ejotes", comida: "Tampiqueña", cena: "Arroz con leche"},
+    {desayuno: "Fruta con Yogurt", comida: "Camarones a la diabla", cena: "Tapioca"},
+    {desayuno: "Huevos ahogados", comida: "Verduras al vapor", cena: "Huevo con arroz"},
   ]  
   //#endregion
 }
